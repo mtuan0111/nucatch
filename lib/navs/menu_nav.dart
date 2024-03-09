5a9dd93233dc488e1dc5fd0b8b2e96ec8c11dacb
Tuan Nguyen <tuannguyen@macromillsea.com>
2024-03-09 19:24:46 +0700

    Making the Setting screen

--- lib/navs/menu_nav.dart
+++ lib/navs/menu_nav.dart
@@ -3,8 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nucatch_with_bloc/blocs/navs/menu/menu_bloc.dart';
 import 'package:nucatch_with_bloc/blocs/navs/menu/menu_event.dart';
 import 'package:nucatch_with_bloc/blocs/navs/menu/menu_state.dart';
-import 'package:nucatch_with_bloc/screens/home_screen.dart';
+import 'package:nucatch_with_bloc/screens/menu_screens/about_screen.dart';
+
+import 'package:nucatch_with_bloc/screens/menu_screens/home_screen.dart';
 import 'package:nucatch_with_bloc/screens/menu_screen.dart';
+import 'package:nucatch_with_bloc/screens/menu_screens/setting_screen.dart';
+import 'package:nucatch_with_bloc/screens/menu_screens/top_score_screen.dart';
 
 class MenuNav extends StatefulWidget {
   const MenuNav({super.key});
@@ -16,26 +20,25 @@ class MenuNav extends StatefulWidget {
 class _MenuNavState extends State<MenuNav> {
   @override
   Widget build(BuildContext context) {
-    bool requesToExits = false;
     return BlocBuilder<MenuBloc, MenuState>(
       builder: ((context, state) => PopScope(
             canPop: false,
             onPopInvoked: (involked) {
               if (state is Menu) {
-                if (!requesToExits) {
-                  requesToExits = true;
-                  SnackBar snackBar = const SnackBar(
-                    content: Text("Tap one more time to exit"),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                } else {
-                  context
-                      .read<MenuBloc>()
-                      .add(SelectOption(option: MenuOption.exit));
-                }
-              } else {
-                requesToExits = false;
+                SnackBar snackBar = SnackBar(
+                  content: const Text("Do you want to close the app?"),
+                  action: SnackBarAction(
+                    label: 'Yes',
+                    onPressed: () {
+                      context
+                          .read<MenuBloc>()
+                          .add(SelectOption(option: MenuOption.exit));
+                    },
+                  ),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
               }
+
               context.read<MenuBloc>().add(SelectOption(option: null));
             },
             child: Navigator(
@@ -53,15 +56,15 @@ class _MenuNavState extends State<MenuNav> {
                   ),
                 if (state is TopScore)
                   const MaterialPage(
-                    child: Text("Top Score"),
+                    child: TopScoreScreen(),
                   ),
                 if (state is Setting)
                   const MaterialPage(
-                    child: Text("Setting"),
+                    child: SettingScreen(),
                   ),
                 if (state is About)
                   const MaterialPage(
-                    child: Text("About"),
+                    child: AboutScreen(),
                   ),
               ],
             ),
